ff6ff2990d01858ad000a4113f5fa9527889e43d
hihijenny <jenny_nijhof@hotmail.com>
2021-02-16 13:20:10 +0100

    made a static html page

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require ('express');
 const app = express();
-const port = 3000; 
+const port = 8000; 
 
 app
   .use(express.static("public"))
   .set("views", "view")
 
-app.use(function(req, res) {
-    res.send("404: Page not found", 404);
-  });
+// app.use(function(req, res) {
+//     res.send("404: Page not found", 404);
+//   });
   
+app.use(function(req, res, next) {
+  res.status(404).sendFile('404.html', {root: public});
+});
+
   app.listen(port, () => console.log(`app running on port: ${port}`));
