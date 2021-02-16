8642f50f0d7abe8699a5e2da1db130b8e264cf68
hihijenny <jenny_nijhof@hotmail.com>
2021-02-16 13:21:33 +0100

    made different route for handling static html page

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ app
   .use(express.static("public"))
   .set("views", "view")
 
-// app.use(function(req, res) {
-//     res.send("404: Page not found", 404);
-//   });
-  
 app.use(function(req, res, next) {
-  res.status(404).sendFile('404.html', {root: public});
+  res.status(404).sendFile('404.html', {root: "public"});
 });
 
   app.listen(port, () => console.log(`app running on port: ${port}`));
