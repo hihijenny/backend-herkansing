9e42852be6c42df29a691920cedd3406e6787a12
hihijenny <jenny_nijhof@hotmail.com>
2021-02-16 13:06:11 +0100

    added static route

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require ('express');
 const app = express();
 const port = 3000; 
 
-app.get('/', (req, res) => {
-    res.send("hi there")
-});
+app
+  .use(express.static("public"))
+  .set("views", "view")
+
+app.use(function(req, res) {
+    res.send("404: Page not found", 404);
+  });
+  
+  app.listen(port, () => console.log(`app running on port: ${port}`));
